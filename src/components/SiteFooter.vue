744e7ad60da51e3621db75359e92bc4a493c7d72
matias.wcanvas <matias@wcanvas.com>
2021-07-04 11:07:19 -0300

    Changes to cv link

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -10,7 +10,7 @@
           <div class="col-md-4">
             <a style="align-self:center" target="_blank" href="https://github.com/matiasdavidson"><img class="header-icon footer-icon on-hover-zoom" src="../../public/img/GitHub-Mark-Light-120px-plus.png" alt=""></a>
             <a style="align-self:center" target="_blank" href="https://www.linkedin.com/in/matias-davidson/"><img class="header-icon footer-icon on-hover-zoom" src="../../public/img/linkedin-4-512.png" alt=""></a>
-            <a target="_blank" style="align-self:center" href="https://drive.google.com/file/d/1I68KR6Qh1yPieUZXuiH-xGCB3g67H2eA/view?usp=sharing" ><img class="header-icon footer-icon on-hover-zoom" src="../../public/img/cv-icon.png" alt=""></a>
+            <a target="_blank" style="align-self:center" href="https://drive.google.com/file/d/15uXE7TxMIUSj433kxTiQhDMmw74ez7CO/view?usp=sharing" ><img class="header-icon footer-icon on-hover-zoom" src="../../public/img/cv-icon.png" alt=""></a>
           </div>
         </div>
       </div>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,11 +1,11 @@
 <template>
-      <div class="container">
-        <div class="intro-text">
-          <div class="intro-lead-in">Hi! I'm Matias Davidson</div>
-          <div class="intro-heading">Front End Developer</div>
-          <img src="../assets/img/scroll.png" alt="scroll" class="scroll-icon">
-        </div>
-      </div>
+  <div class="container">
+    <div class="intro-text">
+      <div class="intro-lead-in">Hi! I'm Matias Davidson</div>
+      <div class="intro-heading">Front End Developer</div>
+      <img src="../assets/img/scroll.png" alt="scroll" class="scroll-icon">
+    </div>
+  </div>
 </template>
 
 <script>
